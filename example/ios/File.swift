570e2df093ce4db3684ad3b5113c6289d7256408
Bhola Kumar <93245523+bhola-t2b@users.noreply.github.com>
2024-07-16 01:48:36 +0530

    chore: initial commit

---
+++ example/ios/File.swift
@@ -0,0 +1,6 @@
+//
+//  File.swift
+//  CryptoNativeExample
+//
+
+import Foundation
---
+++ ios/CryptoNative.swift
@@ -0,0 +1,8 @@
+@objc(CryptoNative)
+class CryptoNative: NSObject {
+
+  @objc(multiply:withB:withResolver:withRejecter:)
+  func multiply(a: Float, b: Float, resolve:RCTPromiseResolveBlock,reject:RCTPromiseRejectBlock) -> Void {
+    resolve(a*b)
+  }
+}
